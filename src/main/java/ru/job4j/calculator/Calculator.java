2679335c4c4fa9a21e35665a805de0e8059b50db
Markesoon <saiferdk@mail.ru>
2023-04-06 21:22:25 +0300

    3. Арифметические операции. [#185 #320688]

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int four = 4;
+        int five = 5;
+        int six = 6;
+        int onePlusTwo = one + two;
+        int sixDivtwo = six / two;
+        int fiveMinustwo = five - two;
+        int fourTimetwo = four * two;
+        System.out.println(sixDivtwo);
+        System.out.println(fiveMinustwo);
+        System.out.println(fourTimetwo);
+        System.out.println(onePlusTwo);
+    }
+}
